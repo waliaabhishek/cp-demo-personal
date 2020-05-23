6cfcc78b9736bca6ebf840ccac3ef8de311e241b
Abhishek Walia <abhishek@confluent.io>
2020-05-22 23:11:02 -0700

    Added TODO

--- scripts/start_addon.sh
+++ scripts/start_addon.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO : Work on determining if the docker Override file is active or not and only then execute the following docker-compose commands.
 echo
 echo "Start the Kafka JMX Metrics Collection application"
 docker-compose up -d jmx-data-poller prometheus grafana splunk 
