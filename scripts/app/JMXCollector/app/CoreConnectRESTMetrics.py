6daa220f94399ab75b9ded92d590aece0e06fce0
Abhishek Walia <abhishek@confluent.io>
2020-05-26 12:23:51 -0700

    Reformatted the file and added some more details to the main function

--- scripts/app/JMXCollector/app/CoreConnectRESTMetrics.py
+++ scripts/app/JMXCollector/app/CoreConnectRESTMetrics.py
@@ -25,13 +25,14 @@ CORE_ENDPOINTS = ["/connectors",
                   "/connectors/{name}/status"]
 
 
-# Async Thread executor for making calls to the REST API. 
+# Async Thread executor for making calls to the REST API.
 # The input_uri takes a list of uri as an argument to call and calls them parallely
 # The result is returned back as a list of data responses.
 async def invoke_urls(input_uri: list, **kwargs):
     return_data = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=CONCURRENT_THREADS) as executor:
-        future_to_url = (executor.submit(invoke_call, uri, user="superUser", password="superUser", kwargs=kwargs) for uri in input_uri)
+        future_to_url = (executor.submit(invoke_call, uri, user="superUser",
+                                         password="superUser", kwargs=kwargs) for uri in input_uri)
         for future in concurrent.futures.as_completed(future_to_url):
             try:
                 contents = future.result()
@@ -41,7 +42,7 @@ async def invoke_urls(input_uri: list, **kwargs):
     return return_data
 
 
-# Invoke the call for the input_uri passed. 
+# Invoke the call for the input_uri passed.
 # This will setup the request session and calls the url.
 # Also does basic auth if AUTH_ENABLED is True.
 def invoke_call(input_uri, **kwargs):
@@ -94,7 +95,8 @@ def generate_jmx_metrics_object(generate_metrics_object_result: list):
                 + "type=" + JMX_METRICS_TYPE + "," \
                 + JMX_METRICS_ATTR_CONNECTOR_NAME + "=" + item[JMX_METRICS_ATTR_CONNECTOR_NAME] + "," \
                 + JMX_METRICS_ATTR_CONNECTOR_TYPE + "=" + item[JMX_METRICS_ATTR_CONNECTOR_TYPE] + "," \
-                + JMX_METRICS_ATTR_TASK_ID + "=" + str(item[JMX_METRICS_ATTR_TASK_ID])
+                + JMX_METRICS_ATTR_TASK_ID + "=" + \
+                str(item[JMX_METRICS_ATTR_TASK_ID])
             value_dict[JMX_METRICS_ATTR_CONNECTOR_STATE] = item[JMX_METRICS_ATTR_CONNECTOR_STATE]
             value_dict[JMX_METRICS_ATTR_TASK_STATE] = item[JMX_METRICS_ATTR_TASK_STATE]
             value_dict[JMX_METRICS_ATTR_TASK_WORKER_ID] = item[JMX_METRICS_ATTR_TASK_WORKER_ID]
@@ -102,27 +104,37 @@ def generate_jmx_metrics_object(generate_metrics_object_result: list):
             key_string = JMX_METRICS_BEAN_NAME + ":" \
                 + "type=" + JMX_METRICS_TYPE + "," \
                 + JMX_METRICS_ATTR_CONNECTOR_NAME + "=" + item[JMX_METRICS_ATTR_CONNECTOR_NAME] + "," \
-                + JMX_METRICS_ATTR_CONNECTOR_TYPE + "=" + item[JMX_METRICS_ATTR_CONNECTOR_TYPE]
+                + JMX_METRICS_ATTR_CONNECTOR_TYPE + "=" + \
+                item[JMX_METRICS_ATTR_CONNECTOR_TYPE]
             value_dict[JMX_METRICS_ATTR_CONNECTOR_STATE] = item[JMX_METRICS_ATTR_CONNECTOR_STATE]
         jmx_metrics_data[key_string] = value_dict
     return jmx_metrics_data
 
 
 if __name__ == "__main__":
+    # Setup the loop to run the REST calls
     loop = asyncio.get_event_loop()
+    # Get all deployed connectors
     connectors_list = loop.run_until_complete(invoke_urls([CORE_ENDPOINTS[0]]))
-    connectors_status = [CORE_ENDPOINTS[1].replace("{name}", k) for k in itertools.chain.from_iterable(connectors_list)]
-    connectors_status_result = loop.run_until_complete(invoke_urls(connectors_status))
+    # Render Connetor REST URI
+    connectors_status = [CORE_ENDPOINTS[1].replace(
+        "{name}", k) for k in itertools.chain.from_iterable(connectors_list)]
+    # Check Connector Status
+    connectors_status_result = loop.run_until_complete(
+        invoke_urls(connectors_status))
+
+    # Setup a metrics object with all the attributes
     from pprint import pprint as pp
     import json
-    output_object = generate_metrics_object(connector_status_dict=connectors_status_result)
-    # pp(output_object)
+    output_object = generate_metrics_object(
+        connector_status_dict=connectors_status_result)
+
+    # Convert the Data into JMX styled structure used in the JMX Scraper Module
     import JMXScraper
     from urllib.parse import urlparse
     url_details = urlparse(CONNECT_REST_ENDPOINT)
     server_host_name = str(url_details.hostname + ":" + str(url_details.port))
-    formmatted_jmx_data = JMXScraper.internal_get_structured_json_from_response(generate_jmx_metrics_object(output_object),
-                                                                                "localhost:8083",
-                                                                                server_ID="KafkaConnect")
+    formmatted_jmx_data = JMXScraper.internal_get_structured_json_from_response(
+        generate_jmx_metrics_object(output_object), server_host_name, server_ID="KafkaConnect")
     pp(formmatted_jmx_data, width=400)
     loop.close()
