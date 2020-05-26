d598cfd3abddaa149d3dfbb481bb76e1e15bebf8
Abhishek Walia <abhishek@confluent.io>
2020-05-26 12:23:08 -0700

    Formatted the file properly and removed unused function

--- scripts/app/JMXCollector/app/main.py
+++ scripts/app/JMXCollector/app/main.py
@@ -28,20 +28,16 @@ default_JMX_URLs = ["/jolokia/read/java.lang:type=*"]
 ingestion_modules = ["elastic"]
 
 
-def setup_connection():
-    for item in ingestion_modules:
-        if "elastic" == item:
-            ElasticSearchAppender.setup_elastic_connection
-
-
 async def main_loop(calling_object_method, jmx_data_node):
     task_list = []
     for call_type in calling_object_method:
         for data_node in jmx_data_node.values():
             if "elastic" in call_type:
-                task_list.append(loop.run_in_executor(_executor, ElasticSearchAppender.call_elastic_bulk, data_node))
+                task_list.append(loop.run_in_executor(
+                    _executor, ElasticSearchAppender.call_elastic_bulk, data_node))
             if "kafka" in call_type:
-                task_list.append(loop.run_in_executor(_executor, KafkaAppender.produce_messages_to_kafka, data_node, JMXScraper.last_fetch_timestamp, KafkaAppender.DEFAULT_KAFKA_PRODUCER))
+                task_list.append(loop.run_in_executor(_executor, KafkaAppender.produce_messages_to_kafka,
+                                                      data_node, JMXScraper.last_fetch_timestamp, KafkaAppender.DEFAULT_KAFKA_PRODUCER))
     await asyncio.gather(*task_list)
     # if "elastic" in calling_object_method:
     #     start_time = time.perf_counter()
@@ -55,7 +51,8 @@ async def main_loop(calling_object_method, jmx_data_node):
 
 if __name__ == "__main__":
     JMXScraper.setup_everything(url_list, default_JMX_URLs,
-                                poll_wait=(30 if POLL_WAIT_IN_SECS < 30 else POLL_WAIT_IN_SECS),
+                                poll_wait=(30 if POLL_WAIT_IN_SECS <
+                                           30 else POLL_WAIT_IN_SECS),
                                 thread_count=JMX_POLL_CONCURRENT_THREADS)
     print(json.dumps(JMXScraper.url_list, indent=2))
     if ("elastic" in ingestion_modules):
@@ -66,15 +63,17 @@ if __name__ == "__main__":
     if runCode:
         loop = asyncio.get_event_loop()
         while (True):
-            print("Metrics Gather poll session started at time \t" + 
+            print("Metrics Gather poll session started at time \t" +
                   time.strftime("%Y-%m-%d %H:%M:%S"))
             # force_collection = bool(random.random() > 0.5)
             if (JMXScraper.get_metrics(force_metric_collection=False)):
                 print("New data updated in the JMX object. Please retrieve from there")
                 start_time = time.perf_counter()
-                loop.run_until_complete(main_loop(ingestion_modules, JMXScraper.jmx_metrics_data))
+                loop.run_until_complete(
+                    main_loop(ingestion_modules, JMXScraper.jmx_metrics_data))
                 end_time = time.perf_counter() - start_time
-                print(f"Ingestion comnplete cycle finished in {end_time:0.2f} seconds.")
+                print(
+                    f"Ingestion comnplete cycle finished in {end_time:0.2f} seconds.")
 
             else:
                 print("No new data received this cycle.Please try again later")
